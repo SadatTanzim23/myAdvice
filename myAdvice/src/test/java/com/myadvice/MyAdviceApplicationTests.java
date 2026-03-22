95bdb7b3157375601d6141c0bd2420eda0b9431f
Ali Al Fattouhi Al Jundi <alfattoa@uwindsor.ca>
2026-03-22 17:59:24 -0400

    Set up Spring Boot project structure

---
+++ myAdvice/src/test/java/com/myadvice/MyAdviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myadvice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyAdviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
