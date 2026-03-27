0e1b5709d91163b3bae72b16cb8b940ca07a2985
Ali Al Fattouhi Al Jundi <alfattoa@uwindsor.ca>
2026-03-27 08:34:16 -0400

    Add Endpoints for Report management methods

--- myAdvice/src/main/java/com/myadvice/controller/ReportController.java
+++ myAdvice/src/main/java/com/myadvice/controller/ReportController.java
@@ -1,4 +1,48 @@
 package com.myadvice.controller;
 
+import com.myadvice.dto.AppointmentCountsDto;
+import com.myadvice.dto.EntityAppointmentCountDto;
+import com.myadvice.model.Faculty;
+import com.myadvice.model.Student;
+import com.myadvice.service.ReportService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/reports")
+@CrossOrigin(origins = "*")
 public class ReportController {
+
+	@Autowired
+	private ReportService reportService;
+
+	@GetMapping("/students")
+	public List<Student> getAllStudents() {
+		return reportService.getAllStudents();
+	}
+
+	@GetMapping("/faculty")
+	public List<Faculty> getAllFaculty() {
+		return reportService.getAllFaculty();
+	}
+
+	@GetMapping("/students/most-appointments")
+	public List<EntityAppointmentCountDto> getStudentsWithMostAppointments() {
+		return reportService.getStudentsWithMostAppointments();
+	}
+
+	@GetMapping("/faculty/most-appointments")
+	public List<EntityAppointmentCountDto> getFacultyWithMostAppointments() {
+		return reportService.getFacultyWithMostAppointments();
+	}
+
+	@GetMapping("/appointments/counts")
+	public AppointmentCountsDto getAppointmentCounts() {
+		return reportService.getAppointmentCounts();
+	}
 }
