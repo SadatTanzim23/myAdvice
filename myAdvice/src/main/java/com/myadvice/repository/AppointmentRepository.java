150c1a321e5075082c4028f6835d702fbbc3922e
Ali Al Fattouhi Al Jundi <alfattoa@uwindsor.ca>
2026-03-27 08:20:25 -0400

    Add Appointment repository to use for appointement management

--- myAdvice/src/main/java/com/myadvice/repository/AppointmentRepository.java
+++ myAdvice/src/main/java/com/myadvice/repository/AppointmentRepository.java
@@ -4,6 +4,7 @@ import com.myadvice.model.Appointment;
 import com.myadvice.model.Faculty;
 import com.myadvice.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -15,4 +16,12 @@ public interface AppointmentRepository extends JpaRepository<Appointment, Long> 
     List<Appointment> findByStudent(Student student);
     //Gets all appointments for a specific faculty member
     List<Appointment> findByFaculty(Faculty faculty);
+
+    @Query("SELECT a.student.id, COUNT(a) FROM Appointment a GROUP BY a.student.id ORDER BY COUNT(a) DESC")
+    List<Object[]> countAppointmentsByStudent();
+
+    @Query("SELECT a.faculty.id, COUNT(a) FROM Appointment a GROUP BY a.faculty.id ORDER BY COUNT(a) DESC")
+    List<Object[]> countAppointmentsByFaculty();
+
+    long countByStatusIgnoreCase(String status);
 }
