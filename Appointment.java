62ed330682486dec06c0d7406f428bea6e8f3661
Ali Al Fattouhi Al Jundi <alfattoa@uwindsor.ca>
2026-03-28 09:10:30 -0400

    Update the Appointment model

---
+++ Appointment.java
@@ -0,0 +1,50 @@
+public class Appointment {
+    private Long id;
+    private Student student;
+    private Faculty faculty;
+    private String dateTime;
+    private String status;
+
+    public Appointment() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public Faculty getFaculty() {
+        return faculty;
+    }
+
+    public void setFaculty(Faculty faculty) {
+        this.faculty = faculty;
+    }
+
+    public String getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(String dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
+
